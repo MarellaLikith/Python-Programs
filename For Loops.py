02716e4dea55d7c9009fceb9fb3e27c79c39ad61
MarellaLikith <54217210+MarellaLikith@users.noreply.github.com>
2020-06-08 00:22:27 +0530

    Python Program

---
+++ For Loops.py
@@ -0,0 +1,14 @@
+#  Syntax of a for loop
+#  my_iterable = [1,2,3]
+#  for item_name in my_iterable:
+#     print(item_name)
+#  >>1
+#  >>2
+#  >>3
+mylist = [1,2,3,4,5,6,7,8,9,10]
+for num in mylist:
+    print(num)
+
+for num in mylist:
+    print('Hello')
+
